03b3d3f349eae586cad8176e2788f05a20dc52a1
akshada242 <131636966+akshada242@users.noreply.github.com>
2024-09-22 00:56:47 +0530

    Create sortarray.java

---
+++ sortarray.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.println("Before sorting:");
+        printArray(array);
+
+        Arrays.sort(array);
+
+        System.out.println("After sorting:");
+        printArray(array);
+    }
+
+    public static void printArray(int[] array) {
+        for (int element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+}
