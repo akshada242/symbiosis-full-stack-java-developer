2a9b7bd6a357a1909153606a8a4b1e9e8b0b31ba
akshada242 <131636966+akshada242@users.noreply.github.com>
2024-09-22 00:45:40 +0530

    Create student.java

---
+++ student.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+public class student {
+	public static void main(String[] args) {
+		
+		System.out.println("ak");
+		int a,b,c;
+		int num1=10;int num2=20;
+		int x=0,y=6,z=10;
+		x=y+z;
+		System.out.println("sum is "+x);
+		
+		Scanner sc=new Scanner(System.in);
+		a=sc .nextInt();
+		b=sc .nextInt();
+		c=a*b;
+		System.out.println(c);
+	}
+
+}
